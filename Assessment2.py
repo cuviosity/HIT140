5d34631082c991a96e661b66eaabd92fabcb0d46
s383733 <s383733@students.cdu.edu.au>
2025-08-17 16:15:50 +1000

    Start work on descriptive statistics

--- Assessment2.py
+++ Assessment2.py
@@ -7,3 +7,11 @@ import statsmodels.stats.weightstats as stm
 df_dataset1 = pd.read_csv('dataset1.csv')
 df_dataset2 = pd.read_csv('dataset2.csv')
 
+# Do bats perceive rats not just as competitors for food but also as potential predators?
+
+df_dataset1_risk = df_dataset1['risk']
+df_dataset1_reward = df_dataset1['reward']
+
+x_bar_risk = np.mean(df_dataset1_risk)
+x_bar_reward = np.mean(df_dataset1_reward)
+
