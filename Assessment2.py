3554d07645e56b4f3b5f644815b1ce7fb3e58d39
s383733 <s383733@students.cdu.edu.au>
2025-08-17 15:46:46 +1000

    Initial test commit

---
+++ Assessment2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as st
+import math
+import statsmodels.stats.weightstats as stm
+
+df_dataset1 = pd.read_csv('dataset1.csv')
+df_dataset2 = pd.read_csv('dataset2.csv')
+
