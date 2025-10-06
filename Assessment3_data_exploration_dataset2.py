e03fb0c7d363537d0b7a4b2eabfc47ac1436c970
s383733 <s383733@students.cdu.edu.au>
2025-10-06 17:53:30 +1100

    Beginning work on box plots for data exploration

--- Assessment3_data_exploration_dataset2.py
+++ Assessment3_data_exploration_dataset2.py
@@ -13,42 +13,47 @@ df['time'] = pd.to_datetime(df['time'], format='%d/%m/%Y %H:%M')
 df['month_year'] = df['time'].dt.strftime('%b %Y')  # e.g., Dec 2017
 
 
-# Pairplot
-sns.pairplot(df)
-plt.suptitle('Time to Approach Food vs Seconds After Rat Arrival by Month', y=1.02)
-plt.tight_layout()
-plt.show()  
+# # Pairplot
+# sns.pairplot(df)
+# plt.suptitle('Time to Approach Food vs Seconds After Rat Arrival by Month', y=1.02)
+# plt.tight_layout()
+# plt.show()  
+# 
+# # Scatterplot
+# fig, ax = plt.subplots(figsize=(12, 6))
+# sns.scatterplot(data=df, x='time', y='bat_landing_number', color='blue', legend=False, ax= ax)
+# plt.xlabel('Time')
+# plt.ylabel('Bat Landing Number')
+# plt.title('Bat Landing Number Over Time')
+# plt.xticks(rotation=45) # Make x-axis labels more readable
+# plt.tight_layout()
+# plt.show()
+# 
+# # Relational Plots
+# sns.relplot(data=df, x='time', y='bat_landing_number', hue='food_availability', col='rat_arrival_number', kind='scatter', col_wrap=3, height=4, aspect=1)
+# plt.show()
+# 
+# # Remove non-numberic columns from dataframe
+# numeric_df = df.select_dtypes(include=[np.number])
+# 
+# # Plot correlation matrix
+# corr = numeric_df.corr()
+# 
+# # Plot heatmap
+# ax = sns.heatmap(corr,
+#                  vmin=-1, vmax=1, center=0, 
+#                  cmap=sns.diverging_palette(20, 220, n=200),
+#                  square=False,
+#                  annot=True)
+# ax.set_xticklabels(
+#     ax.get_xticklabels(),
+#     rotation=45,
+#     horizontalalignment='right'
+# )
+# plt.title('Dataset 2 Correlation Matrix')
+# plt.show()
 
-# Scatterplot
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.scatterplot(data=df, x='time', y='bat_landing_number', color='blue', legend=False, ax= ax)
-plt.xlabel('Time')
-plt.ylabel('Bat Landing Number')
-plt.title('Bat Landing Number Over Time')
-plt.xticks(rotation=45) # Make x-axis labels more readable
-plt.tight_layout()
-plt.show()
-
-# Relational Plots
-sns.relplot(data=df, x='time', y='bat_landing_number', hue='food_availability', col='rat_arrival_number', kind='scatter', col_wrap=3, height=4, aspect=1)
-plt.show()
-
-# Remove non-numberic columns from dataframe
-numeric_df = df.select_dtypes(include=[np.number])
-
-# Plot correlation matrix
-corr = numeric_df.corr()
-
-# Plot heatmap
-ax = sns.heatmap(corr,
-                 vmin=-1, vmax=1, center=0, 
-                 cmap=sns.diverging_palette(20, 220, n=200),
-                 square=False,
-                 annot=True)
-ax.set_xticklabels(
-    ax.get_xticklabels(),
-    rotation=45,
-    horizontalalignment='right'
-)
-plt.title('Dataset 2 Correlation Matrix')
+# Create a collection of boxplots
+ax = sns.boxplot(data=df, orient='h', palette='Set2', whis=1.5)
+plt.title('Boxplots of Numeric Variables in Dataset 2')
 plt.show()
