82ddba911df1c0bcad7c41e433bde8569e015671
s383733 <s383733@students.cdu.edu.au>
2025-10-07 11:37:22 +1100

    First two hypothesis testing completed plus imagery

--- Assessment 3/Assessment3_data_exploration_dataset2.py
+++ Assessment 3/Assessment3_data_exploration_dataset2.py
@@ -52,18 +52,67 @@ df['month_year'] = df['time'].dt.strftime('%b %Y')  # e.g., Dec 2017
 # )
 # plt.title('Dataset 2 Correlation Matrix')
 # plt.show()
-
+# 
 # Create a collection of boxplots
 # ax = sns.boxplot(data=df, orient='h', palette='Set2', whis=1.5)
 # plt.title('Boxplots of Numeric Variables in Dataset 2')
 # plt.show()
-
+# 
 # # Create a specific boxplot for bat landing number
 # ax = sns.boxplot(x='bat_landing_number', data=df, palette='Set3', whis=1.5)
 # plt.title('Boxplot of Bat Landing Number')
 # plt.xlabel('Bat Landing Number')
 # plt.show()
 # This shows that there are outliers in bat landing number on the upper end. 
+# 
+# # In line with hypothesis test 1. create two boxplots for bat landing numbers. One with no rats arriving, and one with rats arriving. Both boxplots should be on the same image.
+# # First split the dataframe into two dataframes. One for rat arrival number = 0, and one for rat arrival number > 0.   
+df_no_rats = df[df['rat_arrival_number'] == 0]
+df_with_rats = df[df['rat_arrival_number'] > 0]
+# # Create boxplot for bat landing number where one boxplot is for rat arrivals = 0, and one boxplot is for rats arrivals >0.
+# sns.boxplot(
+#     x=(df['rat_arrival_number'] > 0).map({False: 'No Rats Arriving', True: 'Rats Arriving'}),
+#     y=df['bat_landing_number'],
+#     palette='Set3',
+#     whis=1.5
+# )
+# plt.xticks([0, 1], ['No Rats Arriving', 'Rats Arriving'])
+# plt.title('Boxplot of Bat Landing Number by Rat Arrival Status')
+# plt.xlabel('Rat Arrival Status')
+# plt.ylabel('Bat Landing Number')
+# plt.show()
+# 
+# The next figures address hypothesis test 2.
+df_less_than_median = df_with_rats[df_with_rats['rat_minutes'] <= df_with_rats['rat_minutes'].median()]
+df_greater_than_median = df_with_rats[df_with_rats['rat_minutes'] > df_with_rats['rat_minutes'].median()]
+# # Create boxplot for bat landing number where one boxplot is for rat minutes less than or equal to the median, 
+# # and one boxplot is for rat minutes greater than the median.
+# sns.boxplot(
+#     x=(df_with_rats['rat_minutes'] > df_with_rats['rat_minutes'].median()).map({False: 'Rat Minutes <= Median', True: 'Rat Minutes > Median'}),
+#     y=df_with_rats['bat_landing_number'],
+#     palette='Set3',
+#     whis=1.5
+# )
+# plt.xticks([0, 1], ['Rat Minutes <= Median', 'Rat Minutes > Median'])
+# plt.title('Boxplot of Bat Landing Number by Rat Minutes (for Observations with Rats Arriving)')
+# plt.xlabel('Rat Minutes Category')
+# plt.ylabel('Bat Landing Number')
+# plt.show()
+# 
+# # Next create a scatterplot when rat_minutes≠0 for hypothesis test 2.
+# sns.scatterplot(data=df_with_rats, x='rat_minutes', y='bat_landing_number', color='blue')
+# plt.xlabel('Rat Minutes')
+# plt.ylabel('Bat Landing Number')
+# plt.title('Scatterplot of Bat Landing Number vs Rat Minutes (for Observations with Rats Arriving)')
+# plt.show()
+# 
+# Create boxplot for bat landing number where one boxplot is for no rats arriving, and one boxplot is for rats arriving.
+# plt.figure(figsize=(10, 6))
+# sns.boxplot(x='rat_arrival_number', y='bat_landing_number', data=df, palette='Set3', whis=1.5)
+# plt.title('Boxplot of Bat Landing Number by Rat Arrival Number')
+# plt.xlabel('Rat Arrival Number (0 = No Rats, >0 = Rats Arriving)')
+# plt.ylabel('Bat Landing Number')
+# plt.show()
 
 # # Create a specific boxplot for rat minutes
 # ax = sns.boxplot(x='rat_minutes', data=df, palette='Set3', whis=1.5)   
@@ -78,7 +127,7 @@ df['month_year'] = df['time'].dt.strftime('%b %Y')  # e.g., Dec 2017
 # plt.xlabel('Rat Arrival Number')
 # plt.show()
 # #Due to the large number of zero values(no rat arriving in observation period), the data is right skewed and does not represent a normal box plot.
-
+# 
 # Create a histogram with bat landing number on the y axis and food availability on the x axis
 # plt.figure(figsize=(10, 6))
 # sns.histplot(data=df, x='food_availability', bins=20, kde=True, color='skyblue', edgecolor='black')
---
+++ Assessment 3/Assessment3_dataset2_hypothesis_testing.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.stats.proportion as stm
+import scipy.stats as st
+
+# Load the dataset into python
+df = pd.read_csv('/Users/christoffeljansevanvuuren/Home Documents/University/HIT140/Assessment 2/GitHub/HIT140/Assessment 3/dataset2.csv')
+
+# For hypothesis test 1. we need to determine whether there is an increased level of bat avoidance as the number of rats increases. 
+# To carry out this test we will compare when there are no rats present on the platform to when rats are present on the platform.
+# First we will need to seperate the data into two groups. One dataframe for when there are no rats present (rat arrival number = 0) 
+# and one for when there are rats present (rat arrival number > 0).
+
+df_no_rats = df[df['rat_arrival_number'] == 0]
+df_with_rats = df[df['rat_arrival_number'] > 0]
+
+# Next we will calculate the mean number of bat landings for both dataframes.
+mean_no_rats = df_no_rats['bat_landing_number'].mean()
+mean_with_rats = df_with_rats['bat_landing_number'].mean()
+
+print(f'Mean number of bat landings with no rats: {mean_no_rats}')
+print(f'Mean number of bat landings with rats: {mean_with_rats}')
+
+# # Now we will perform a two-sample t-test to determine if the difference in means is statistically significant given the size of the data sample.
+# t_stats, p_value, dfree = sm.stats.ttest_ind(df_no_rats['bat_landing_number'], df_with_rats['bat_landing_number'])
+# alpha = 0.05
+# print(f'T-stat: {t_stats}, P-val: {p_value}')
+# if p_value < alpha:
+#     print('Reject the null hypothesis: There is a significant difference in bat landings when rats are present.')
+# else:
+#     print('Fail to reject the null hypothesis: There is no significant difference in bat landings with and without rats present.')
+# 
+# # Now run the test with an alpha of 0.01
+# alpha = 0.01
+# if p_value < alpha:
+#     print('Reject the null hypothesis at alpha 0.01: There is a significant difference in bat landings when rats are present.')
+# else:
+#     print('Fail to reject the null hypothesis at alpha 0.01: There is no significant difference in bat landings with and without rats present.')
+
+# Next we will conduct a hypothesis test to determine if there is a decrease in bat landings as the number of rat minutes increases.
+# If the bats perceive the rats as predators, we would expect to see a decrease in bat landings as the number of rat minutes increases.
+# If the bats perceive the rats as competitors, we would expect to see an increase in bat landings as the number of rat minutes increases.
+# We already know from the previous test that there is a difference in bat landings when rat_minutes =0 compared to when rat_minutes > 0.
+# Therefore we will only be testing the data where rat_minutes > 0. 
+# The first test we will do will split the data into two groups. One group where rat_minutes is less than or equal to the median rat_minutes, 
+# and one group where rat_minutes is greater than the median rat_minutes.
+# We will then perform a two-sample t-test to determine if there is a significant difference in bat landings between the two groups.
+print("Median rat minutes:", df_with_rats['rat_minutes'].median())
+df_less_than_median = df_with_rats[df_with_rats['rat_minutes'] <= df_with_rats['rat_minutes'].median()]
+df_greater_than_median = df_with_rats[df_with_rats['rat_minutes'] > df_with_rats['rat_minutes'].median()]
+
+t_stats, p_value, dfree = sm.stats.ttest_ind(df_less_than_median['bat_landing_number'], df_greater_than_median['bat_landing_number'])
+alpha = 0.05
+print(f'T-stat: {t_stats}, P-val: {p_value}')
+if p_value < alpha:
+    print('Reject the null hypothesis: There is a significant difference in bat landings between low and high rat minutes.')
+else:
+    print('Fail to reject the null hypothesis: There is no significant difference in bat landings between low and high rat minutes.')
+
+# # We will use a correlation test to determine if there is a significant correlation between the number of rat minutes and the number of bat landings.
+# correlation, p_value = st.pearsonr(df_with_rats['rat_minutes'], df_with_rats['bat_landing_number'])
+# alpha = 0.05
+# print(f'Correlation: {correlation}, P-val: {p_value}')
+# if p_value < alpha:
+#     print('Reject the null hypothesis: There is a significant correlation between rat minutes and bat landings.')
+# else:
+#     print('Fail to reject the null hypothesis: There is no significant correlation between rat minutes and bat landings.')  
